9bb3720ef69d13d312fba356ad399bc8922d356f
s185736 <65380030+s185736@users.noreply.github.com>
2022-02-17 15:21:18 +0100

    Opdatering.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module roborally {
+
+    requires javafx.graphics;
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires annotations;
+
+    opens sample;
+}
