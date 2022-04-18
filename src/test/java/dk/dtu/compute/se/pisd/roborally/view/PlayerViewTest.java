3884bee5a6aa51630f69f8bc309669d1f2a61762
User <65380030+s185736@users.noreply.github.com>
2022-04-18 17:33:15 +0200

    Added some start..

---
+++ src/test/java/dk/dtu/compute/se/pisd/roborally/view/PlayerViewTest.java
@@ -0,0 +1,14 @@
+package dk.dtu.compute.se.pisd.roborally.view;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerViewTest {
+
+    @Test
+    void updateView() {
+
+    }
+
+}
