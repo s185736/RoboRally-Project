ba6774798c7cd2fa0d036bd6dc500d170cf0e3e4
User <65380030+s185736@users.noreply.github.com>
2022-04-20 23:28:49 +0200

    Implemented Antenna Class.

---
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/Antenna.java
@@ -0,0 +1,20 @@
+package dk.dtu.compute.se.pisd.roborally.model;
+
+public class Antenna {
+
+    /*Defining the variables, we're going to use.*/
+    public final Board board;
+    public final int x;
+    public final int y;
+
+    /**
+     * @param board
+     * @param x
+     * @param y
+     */
+    public Antenna(Board board, int x, int y) {
+        this.board = board;
+        this.x = x;
+        this.y = y;
+    }
+}
