2f2f4b5a1c9af315266eda4b0065fa632cbf99d7
User <65380030+s185736@users.noreply.github.com>
2022-06-10 02:55:26 +0200

    Made a start on Antenna, and made some small updates and changes.

--- src/main/java/dk/dtu/compute/se/pisd/roborally/view/board/Controller.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/view/board/Controller.java
@@ -5,7 +5,9 @@ import dk.dtu.compute.se.pisd.roborally.model.subject.Board;
 /**
  * ...
  *
- * @author
+ *  @author Sammy Chauhan, s191181@dtu.dk
+ *  @author Azmi Uslu, s185736@dtu.dk
+ *  @author Malaz Alzarrad, s180424@dtu.dk
  *
  */
 
